c8085ef677a644aafcd3f56ab79e3a8d939f5bde
Leon Si <leonzalion@gmail.com>
2020-05-29 16:33:16 -0400

    hi

--- GamePanel.java
+++ GamePanel.java
@@ -5,122 +5,122 @@ import java.io.*;
 import javax.imageio.ImageIO;
 
 class GamePanel extends JPanel implements KeyListener, Runnable {
-	private double blockWidth, blockHeight;
-	private Thread thread;
-	private long prevTime, diffTime;
-	//private Monster[] monsters;
-	private Player player;
-	boolean[] keyIsPressed;
-	private int[][] map = {
-		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-		{ 1, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
-		{ 1, 0, 1, 1, 0, 3, 0, 0, 0, 1 },
-		{ 1, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
-		{ 1, 1, 1, 1, 0, 0, 0, 1, 0, 1 },
-		{ 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
-		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-		{ 1, 0, 0, 1, 1, 1, 0, 0, 1, 1 },
-		{ 1, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
-		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
-	};
+  private double blockWidth, blockHeight;
+  private Thread thread;
+  private long prevTime, diffTime;
+  //private Monster[] monsters;
+  private Player player;
+  boolean[] keyIsPressed;
+  private int[][] map = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
+    { 1, 0, 1, 1, 0, 3, 0, 0, 0, 1 },
+    { 1, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
+    { 1, 1, 1, 1, 0, 0, 0, 1, 0, 1 },
+    { 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 1, 1, 1, 0, 0, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+  };
 
-	public void start() {
-		thread = new Thread(this);
-		thread.start();
+  public void start() {
+    thread = new Thread(this);
+    thread.start();
 
-		// initialize block size
-		blockWidth=(double)getWidth()/map[0].length;
-		blockHeight=(double)getHeight()/map.length;
+    // initialize block size
+    blockWidth=(double)getWidth()/map[0].length;
+    blockHeight=(double)getHeight()/map.length;
 
-		// find player's starting position in map and create player there
-		for (int i=0;i<map.length;++i) {
-			for (int j=0;j<map[0].length;++j) {
-				if (map[i][j]==3) {
-					try { player = new Player((int)(j*blockWidth),(int)(i*blockHeight), ImageIO.read(new File("images"+File.separator+"player1.png"))); }
-					catch (IOException e) { e.printStackTrace();}
-				}
-			}
-		}
-	}
+    // find player's starting position in map and create player there
+    for (int i=0;i<map.length;++i) {
+      for (int j=0;j<map[0].length;++j) {
+        if (map[i][j]==3) {
+          try { player = new Player((int)(j*blockWidth),(int)(i*blockHeight), ImageIO.read(new File("images"+File.separator+"player1.png"))); }
+          catch (IOException e) { e.printStackTrace();}
+        }
+      }
+    }
+  }
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if (isVisible()) {
-			if (e.getKeyCode() == KeyEvent.VK_UP) keyIsPressed[Direction.UP]=true;
-			else if (e.getKeyCode() == KeyEvent.VK_DOWN) keyIsPressed[Direction.DOWN]=true;
-			else if (e.getKeyCode() == KeyEvent.VK_LEFT) keyIsPressed[Direction.LEFT]=true;
-			else if (e.getKeyCode() == KeyEvent.VK_RIGHT) keyIsPressed[Direction.RIGHT]=true;
-		}
-	}
+  @Override
+  public void keyPressed(KeyEvent e) {
+    if (isVisible()) {
+      if (e.getKeyCode() == KeyEvent.VK_UP) keyIsPressed[Direction.UP]=true;
+      else if (e.getKeyCode() == KeyEvent.VK_DOWN) keyIsPressed[Direction.DOWN]=true;
+      else if (e.getKeyCode() == KeyEvent.VK_LEFT) keyIsPressed[Direction.LEFT]=true;
+      else if (e.getKeyCode() == KeyEvent.VK_RIGHT) keyIsPressed[Direction.RIGHT]=true;
+    }
+  }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		if (isVisible()) {
-			if (e.getKeyCode() == KeyEvent.VK_UP) keyIsPressed[Direction.UP]=false;
-			else if (e.getKeyCode() == KeyEvent.VK_DOWN) keyIsPressed[Direction.DOWN]=false;
-			else if (e.getKeyCode() == KeyEvent.VK_LEFT) keyIsPressed[Direction.LEFT]=false;
-			else if (e.getKeyCode() == KeyEvent.VK_RIGHT) keyIsPressed[Direction.RIGHT]=false;
-		}
-	}
+  @Override
+  public void keyReleased(KeyEvent e) {
+    if (isVisible()) {
+      if (e.getKeyCode() == KeyEvent.VK_UP) keyIsPressed[Direction.UP]=false;
+      else if (e.getKeyCode() == KeyEvent.VK_DOWN) keyIsPressed[Direction.DOWN]=false;
+      else if (e.getKeyCode() == KeyEvent.VK_LEFT) keyIsPressed[Direction.LEFT]=false;
+      else if (e.getKeyCode() == KeyEvent.VK_RIGHT) keyIsPressed[Direction.RIGHT]=false;
+    }
+  }
 
-	@Override
-	public void keyTyped(KeyEvent e) {}
+  @Override
+  public void keyTyped(KeyEvent e) {}
 
-	private void paintMap(Graphics g) {
-	  Color oldColor = g.getColor();
-		g.setColor(Color.black);
+  private void paintMap(Graphics g) {
+    Color oldColor = g.getColor();
+    g.setColor(Color.black);
 
-		for (int i=0;i<map.length;++i) {
-			for (int j=0;j<map[i].length;++j) {
-				if (map[i][j]==1) {
-					g.fillRect((int)(j*blockWidth), (int)(i*blockHeight), (int)blockWidth+1, (int)blockHeight+1);
-				}
-			}
-		}
+    for (int i=0;i<map.length;++i) {
+      for (int j=0;j<map[i].length;++j) {
+        if (map[i][j]==1) {
+          g.fillRect((int)(j*blockWidth), (int)(i*blockHeight), (int)blockWidth+1, (int)blockHeight+1);
+        }
+      }
+    }
 
-		g.setColor(oldColor);
-	}
+    g.setColor(oldColor);
+  }
 
-	private boolean isWall(int x, int y) {
-		return map[y][x] == 1;
-	}
+  private boolean isWall(int x, int y) {
+    return map[y][x] == 1;
+  }
 
-	public int numColumns() {
-		return map[0].length;
-	}
+  public int numColumns() {
+    return map[0].length;
+  }
 
-	public int numRows() {
-		return map.length;
-	}
+  public int numRows() {
+    return map.length;
+  }
 
-	public void movePlayer(int dir) {
-		// TODO add collision detection
-		player.walk(dir);
-	}
+  public void movePlayer(int dir) {
+    // TODO add collision detection
+    player.walk(dir);
+  }
 
-	public void movePlayer() {
-		if (keyIsPressed[Direction.UP]) movePlayer(Direction.UP);
-		if (keyIsPressed[Direction.DOWN]) movePlayer(Direction.DOWN);
-		if (keyIsPressed[Direction.LEFT]) movePlayer(Direction.LEFT);
-		if (keyIsPressed[Direction.RIGHT]) movePlayer(Direction.RIGHT);
+  public void movePlayer() {
+    if (keyIsPressed[Direction.UP]) movePlayer(Direction.UP);
+    if (keyIsPressed[Direction.DOWN]) movePlayer(Direction.DOWN);
+    if (keyIsPressed[Direction.LEFT]) movePlayer(Direction.LEFT);
+    if (keyIsPressed[Direction.RIGHT]) movePlayer(Direction.RIGHT);
 
-		if (!keyIsPressed[Direction.UP] && !keyIsPressed[Direction.DOWN] && !keyIsPressed[Direction.LEFT] && !keyIsPressed[Direction.RIGHT])
-			player.stop();
-	}
+    if (!keyIsPressed[Direction.UP] && !keyIsPressed[Direction.DOWN] && !keyIsPressed[Direction.LEFT] && !keyIsPressed[Direction.RIGHT])
+      player.stop();
+  }
 
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
 
-		// resize blocks
-		double blockWidth = (double) getWidth() / numColumns();
-		double blockHeight = (double) getHeight() / numRows();
+    // resize blocks
+    double blockWidth = (double) getWidth() / numColumns();
+    double blockHeight = (double) getHeight() / numRows();
 
-		// draw map
-		this.paintMap(g);
+    // draw map
+    this.paintMap(g);
 
-		// If player intersects a wall, stop them
-		Rectangle playerRect = player.getRect();
+    // If player intersects a wall, stop them
+    Rectangle playerRect = player.getRect();
 
 		/*
 		for (int y = 0; y < numRows(); ++y) {
@@ -137,28 +137,28 @@ class GamePanel extends JPanel implements KeyListener, Runnable {
 		}
 		*/
 
-		// Draw the player
-		g.drawImage(player.getImage(), player.getX(), player.getY(), null);
+    // Draw the player
+    g.drawImage(player.getImage(), player.getX(), player.getY(), null);
 
-	}
+  }
 
-	@Override
-	public void run() {
-		prevTime = System.currentTimeMillis();
+  @Override
+  public void run() {
+    prevTime = System.currentTimeMillis();
 
-		while (true) {
-			movePlayer();
-			repaint();
+    while (true) {
+      movePlayer();
+      repaint();
 
-			diffTime = System.currentTimeMillis() - prevTime;
+      diffTime = System.currentTimeMillis() - prevTime;
 
-			// Sleep for 40 milliseconds with accomodation for the time repaint() and movePlayer() took
-			try { Thread.sleep(40 - diffTime); }
-			catch (InterruptedException e) {}
+      // Sleep for 40 milliseconds with accomodation for the time repaint() and movePlayer() took
+      try { Thread.sleep(40 - diffTime); }
+      catch (InterruptedException e) {}
 
-			prevTime = System.currentTimeMillis();
-		}
+      prevTime = System.currentTimeMillis();
+    }
 
-	}
+  }
 
 }
