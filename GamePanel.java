cc29c5049dab7fb9b90b3acba6383276686969dc
Leon Si <leonzalion@gmail.com>
2020-05-29 14:47:11 -0400

    changes

--- GamePanel.java
+++ GamePanel.java
@@ -10,7 +10,6 @@ class GamePanel extends JPanel implements KeyListener, Runnable {
 	private long prevTime, diffTime;
 	//private Monster[] monsters;
 	private Player player;
-	private Rectangle playerRect;
 	private int[][] map = {
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
@@ -31,7 +30,7 @@ class GamePanel extends JPanel implements KeyListener, Runnable {
 		// initialize block size
 		blockWidth=(double)getWidth()/map[0].length;
 		blockHeight=(double)getHeight()/map.length;
-		
+
 		// find player's starting position in map and create player there
 		for (int i=0;i<map.length;++i) {
 			for (int j=0;j<map[0].length;++j) {
@@ -62,17 +61,10 @@ class GamePanel extends JPanel implements KeyListener, Runnable {
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {}	
+	public void keyTyped(KeyEvent e) {}
 
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-
-		// resize blocks
-		blockWidth=(double)getWidth()/map[0].length;
-		blockHeight=(double)getHeight()/map.length;
-		
-		// draw map
+	private void paintMap(Graphics g) {
+	  Color oldColor = g.getColor();
 		g.setColor(Color.black);
 
 		for (int i=0;i<map.length;++i) {
@@ -83,12 +75,44 @@ class GamePanel extends JPanel implements KeyListener, Runnable {
 			}
 		}
 
+		g.setColor(oldColor);
+	}
+
+	private boolean isWall(int x, int y) {
+		return map[y][x] == 1;
+	}
+
+	public int numColumns() {
+		return map[0].length;
+	}
+
+	public int numRows() {
+		return map.length;
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		// resize blocks
+		double blockWidth = (double) getWidth() / numColumns();
+		double blockHeight = (double) getHeight() / numRows();
+
+		// draw map
+		this.paintMap(g);
+
 		// If player intersects a wall, stop them
-		playerRect = player.getRect();
-		for (int i=0;i<map.length;++i) {
-			for (int j=0;j<map[i].length;++j) {
-				if (map[i][j]==1 && playerRect.intersects(new Rectangle((int)(j*blockWidth), (int)(i*blockHeight), (int)blockWidth, (int)blockHeight))) {
-					player.reverseDir();
+		Rectangle playerRect = player.getRect();
+
+		for (int y = 0; y < map.length; ++y) {
+			for (int x = 0; x < map[y].length; ++x) {
+				Point topLeftPoint = new Point((int) (x * blockWidth), (int) (y * blockHeight));
+				Dimension blockSize = new Dimension((int) blockWidth, (int) blockHeight);
+				Rectangle wallBlockRect = new Rectangle(topLeftPoint, blockSize);
+				if (isWall(x, y) && playerRect.intersects(wallBlockRect)) {
+					// TODO only stop in the direction they intersect in
+					player.stop(0);
+					player.stop(1);
 				}
 			}
 		}
@@ -105,7 +129,7 @@ class GamePanel extends JPanel implements KeyListener, Runnable {
 		prevTime = System.currentTimeMillis();
 
 		while (true) {
-			repaint();	
+			repaint();
 			diffTime = System.currentTimeMillis() - prevTime;
 
 			//Sleep for 40 milliseconds with accomodation for the time repaint() took
