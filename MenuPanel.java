06e0dc8b339375da5a9ecc8e83072b43dfbdd1bb
Leon Si <leonzalion@gmail.com>
2020-06-15 21:17:33 -0400

    changed start menu

--- MenuPanel.java
+++ MenuPanel.java
@@ -1,37 +1,29 @@
 import java.awt.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.File;
 
 class MenuPanel extends JPanel implements ActionListener {
-	private CardLayout cards;
-	private Container pane;
-	private GamePanel game;
-	private JPanel playBtnPnl;
-	private JButton playBtn;
-	private JPanel rulesBtnPnl;
-	private JButton rulesBtn;
+	private final CardLayout cards;
+	private final Container pane;
+	private final GamePanel game;
 
 	MenuPanel(CardLayout cards, Container pane, GamePanel game, double width, double height) {
-		this.cards=cards;
-		this.pane=pane;
-		this.game=game;
-		
+		this.cards = cards;
+		this.pane = pane;
+		this.game = game;
+
     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBackground(Color.blue);
 
-    playBtnPnl = new JPanel();
-		playBtnPnl.setBackground(Color.blue);
-		playBtn = makeButton("PLAY", this);
-		playBtnPnl.add(playBtn);
-
-    rulesBtnPnl = new JPanel();
-		rulesBtnPnl.setBackground(Color.blue);
-		rulesBtn = makeButton("RULES", this);
-		rulesBtnPnl.add(rulesBtn);
+		JPanel playBtnPnl = new PlayButton();
+		JPanel rulesBtnPnl = new RulesButton();
+		TitlePanel titlePanel = new TitlePanel();
 
 		add(Box.createVerticalGlue());
+		add(titlePanel);
     add(playBtnPnl);
-		add(Box.createVerticalGlue());
     add(rulesBtnPnl);
 		add(Box.createVerticalGlue());
 	}
@@ -56,7 +48,89 @@ class MenuPanel extends JPanel implements ActionListener {
 			cards.show(pane, "game");
 			game.requestFocusInWindow();
 		}
-		else
+		else {
 			cards.show(pane, "rules");
+		}
+	}
+}
+
+class TitlePanel extends JPanel {
+	@Override
+	public Dimension getMinimumSize() {
+		return new Dimension(800, 400);
+	}
+
+	@Override
+	public Dimension getMaximumSize() {
+		return new Dimension(800, 400);
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(800, 400);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		try {
+			Image image = ImageIO.read(new File("images/title.png"));
+			int x = (this.getWidth() - image.getWidth(null)) / 2;
+			int y = (this.getHeight() - image.getHeight(null)) / 2;
+			g.drawImage(image, x, y, null);
+		} catch (Exception ignored) {}
+	}
+}
+
+class PlayButton extends JPanel {
+	@Override
+	public Dimension getMinimumSize() {
+		return new Dimension(400, 200);
+	}
+
+	@Override
+	public Dimension getMaximumSize() {
+		return new Dimension(400, 200);
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(400, 200);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		try {
+			Image image = ImageIO.read(new File("images/play.png"));
+			int x = (this.getWidth() - image.getWidth(null)) / 2;
+			int y = (this.getHeight() - image.getHeight(null)) / 2;
+			g.drawImage(image, x, y, null);
+		} catch (Exception ignored) {}
+	}
+}
+
+class RulesButton extends JPanel {
+	@Override
+	public Dimension getMinimumSize() {
+		return new Dimension(400, 200);
+	}
+
+	@Override
+	public Dimension getMaximumSize() {
+		return new Dimension(400, 200);
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(400, 200);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		try {
+			Image image = ImageIO.read(new File("images/rules.png"));
+			int x = (this.getWidth() - image.getWidth(null)) / 2;
+			int y = (this.getHeight() - image.getHeight(null)) / 2;
+			g.drawImage(image, x, y, null);
+		} catch (Exception ignored) {}
 	}
 }
